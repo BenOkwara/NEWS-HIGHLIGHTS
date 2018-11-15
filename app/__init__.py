4e24dee7b2bcb13182daa38f82a58b25f71fc816
BenOkwara <maranathabenson@gmail.com>
2018-11-15 09:43:36 +0300

    Create __init__.py file where we will define Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,17 @@
 from flask  import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
 
+bootstrap = Bootstrap()
+
+def create_app(config_name):
 # Initializing my app.
-app = Flask (__name__,instance_relative_config=True)
+app = Flask (__name__)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+# Creating the app configurations
+app.config.from_object(config_options[config_name])
 
 # Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+bootstrap.init_app(app)
 
-from app import views
+return app
